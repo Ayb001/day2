c86c4f6975794d56863d20d0388d9e86ee775db4
Ayb001 <aghjedimayoub@gmail.com>
2025-09-07 19:46:59 +0100

    Challenge 7: Fonction d'Inversion de Chaîne - inverser et afficher une chaîne de caractères

---
+++ Boucles/Challenge7/Challenge7.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+int n, i;
+unsigned long long a=0, b=1, c;
+printf("Entrer le nombre de termes : ");
+scanf("%d", &n);
+for (i=0; i<n; i++) {
+if (i==0) c=0;
+else if(i==1) c=1;
+else { c=a+b; a=b; b=c; }
+printf("%llu ", c);
+}
+printf("\n");
+return 0;
+}
